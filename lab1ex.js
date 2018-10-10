8d1a6b973c2496f73750144553efdb6fe2f4d0b0
Sivongsa <sivmarb@dunwoody.edu>
2018-10-10 09:44:00 -0500

    adding json lab

---
+++ lab1ex.js
@@ -0,0 +1,170 @@
+var data =[
+  {
+    "burgerJoints": "McDonald's",
+    "numbersOfLocation": 37241,
+    "signatureItem": "Big Mac",
+    "originatedLocation": "Des Plaines, IL",
+    "nationalLocation": "Yes"
+  },
+  {
+    "burgerJoints": "Burger King",
+    "numbersOfLocation": 16717,
+    "signatureItem": "Whopper",
+    "originatedLocation": "Jacksonville, FL",
+    "nationalLocation": "Yes"
+  },
+  {
+    "burgerJoints": "Wendy's",
+    "numbersOfLocation": 6500,
+    "signatureItem": "Dave's Double",
+    "originatedLocation": "Columbus, OH",
+    "nationalLocation": "Yes"
+  },
+  {
+    "burgerJoints": "White Castle",
+    "numbersOfLocation": 420,
+    "signatureItem": "Crave Case",
+    "originatedLocation": "Wichita, KS",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Five Guys",
+    "numbersOfLocation": 1500,
+    "signatureItem": "Double Bacon Cheeseburger",
+    "originatedLocation": "Arlington County, VA",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Whata Burger",
+    "numbersOfLocation": 824,
+    "signatureItem": "Whataburger",
+    "originatedLocation": "Corpus Christi, TX",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "In N' Out",
+    "numbersOfLocation": 335,
+    "signatureItem": "Double Double",
+    "originatedLocation": "Baldwin Park, CA",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Smash Burger",
+    "numbersOfLocation": 370,
+    "signatureItem": "Classic Smash",
+    "originatedLocation": "Denver, CO",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "MOS Burger",
+    "numbersOfLocation": 1730,
+    "signatureItem": "MOS Burger",
+    "originatedLocation": "Tokyo, Japan",
+    "nationalLocation": "Yes"
+  },
+  {
+    "burgerJoints": "Freshness Burger",
+    "numbersOfLocation": 189,
+    "signatureItem": "Classic Burger",
+    "originatedLocation": "Tokyo, Japan",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Lotteria",
+    "numbersOfLocation": 205,
+    "signatureItem": "Teriyaki Burger",
+    "originatedLocation": "Tokyo, Japan",
+    "nationalLocation": "Yes"
+  },
+  {
+    "burgerJoints": "Fuddrucker's",
+    "numbersOfLocation": 336,
+    "signatureItem": "Build Your Burger",
+    "originatedLocation": "San Antonio, TX",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Portillo's",
+    "numbersOfLocation": 58,
+    "signatureItem": "Bacon Cheeseburger",
+    "originatedLocation": "Villa Park, IL",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Culver's",
+    "numbersOfLocation": 669,
+    "signatureItem": "Butterburgers",
+    "originatedLocation": "Sauk City, WI",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Sonic's",
+    "numbersOfLocation": 3500,
+    "signatureItem": "SuperSONIC burger",
+    "originatedLocation": "Shawnee, OK",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Steak N' Shake",
+    "numbersOfLocation": 550,
+    "signatureItem": "Prime Frisco Steakburger",
+    "originatedLocation": "Normal, IL",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Jollibee",
+    "numbersOfLocation": 1200,
+    "signatureItem": "Big Yum",
+    "originatedLocation": "Pasig, Philippines",
+    "nationalLocation": "Yes"
+  },
+  {
+    "burgerJoints": "Jack in a Box",
+    "numbersOfLocation": 2200,
+    "signatureItem": "All-American Ribeye Burger",
+    "originatedLocation": "San Diego, CA",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Carl's Jr.",
+    "numbersOfLocation": 1100,
+    "signatureItem": "Famous Star",
+    "originatedLocation": "Los Angeles, CA",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Dairy Queen",
+    "numbersOfLocation": 6400,
+    "signatureItem": "Deluxe Cheeseburger",
+    "originatedLocation": "Joliet, IL",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Burger Time",
+    "numbersOfLocation": 8,
+    "signatureItem": "Bigger Burger",
+    "originatedLocation": "West Fargo, ND",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "A&W",
+    "numbersOfLocation": 1100,
+    "signatureItem": "Papa Burger",
+    "originatedLocation": "Lodi, CA",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Hardee's",
+    "numbersOfLocation": 1944,
+    "signatureItem": "1/3 lbs. burger",
+    "originatedLocation": "Franklin, TN",
+    "nationalLocation": "No"
+  },
+  {
+    "burgerJoints": "Hungry Jack's",
+    "numbersOfLocation": 390,
+    "signatureItem": "The Aussie",
+    "originatedLocation": "Sydney, Australia",
+    "nationalLocation": "No"
+  }
+]
